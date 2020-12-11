608d845f688dd26a1912e6d2e75d453a02152016
Evgeniy Fedina <fedina.er@gmail.com>
2020-12-12 01:58:10 +0200

    arrowColor change

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.portfolio-slider__items').owlCarousel({
         margin:10,
         nav:true,
-        navText: ["<img src='./images/arrow-left.svg'>","<img src='./images/arrow-right.svg'>"],
+        navText: ['<svg width="16" height="28" viewBox="0 0 16 28" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M14 26L2 14L14 2" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg>',"<svg width='16' height='28' viewBox='0 0 16 28' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M2 26L14 14L2 2' stroke='white' stroke-width='3' stroke-linecap='round' stroke-linejoin='round'/></svg>"],
         URLhashListener:true,
         startPosition: 'URLHash',
         responsive:{
@@ -22,7 +22,6 @@ $(document).ready(function(){
     $('.comments-slider__items').owlCarousel({
         margin:10,
         nav:true,
-        // navText: ["<img src='../images/arrow-left.svg'>","<img src='../images/arrow-right.svg'>"],
         navText: ['<svg width="16" height="28" viewBox="0 0 16 28" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M14 26L2 14L14 2" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg>',"<svg width='16' height='28' viewBox='0 0 16 28' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M2 26L14 14L2 2' stroke='white' stroke-width='3' stroke-linecap='round' stroke-linejoin='round'/></svg>"],
         responsive:{
             0:{
